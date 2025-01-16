191115f802a9b3ef65d47b3b514ae01ace3b59ff
wisuwat <wisuwatpond01@gmail.com>
2025-01-16 15:32:09 +0700

    Generate Random Numbers

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,19 @@ import Die from './components/Die'
 
 function App() {
 
+  function generateAllNewDice() {
+    // return new Array(10)
+    //   .fill(0)
+    //   .map(() => Math.floor(Math.random() * 6) + 1)
+
+    const dice = [];
+    for(let i = 0; i < 10; i++) {
+      const random = Math.floor(Math.random() * 6) + 1
+      dice.push(random)
+    }
+    return dice
+  }
+
   return (
     <main>
       <div className='container'>
