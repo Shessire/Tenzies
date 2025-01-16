edfc421322cff3d467d40e40bd932993cb6bb188
wisuwat <wisuwatpond01@gmail.com>
2025-01-16 15:14:27 +0700

    Add Style

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,22 @@
-
 import './App.css'
+import Die from './components/Die'
 
 function App() {
 
   return (
     <main>
+      <div className='container'>
+        <Die value={1}/>
+        <Die value={1}/>
+        <Die value={1}/>
+        <Die value={1}/>
+        <Die value={1}/>
+        <Die value={1}/>
+        <Die value={1}/>
+        <Die value={1}/>
+        <Die value={1}/>
+        <Die value={1}/>
+      </div>
     </main>
   )
 }
---
+++ src/components/Die.jsx
@@ -0,0 +1,9 @@
+function Die ({ value }) {
+    return (
+        <button>
+            {value}
+        </button>
+    )
+}
+
+export default Die;
