8abf0240c8095f64e3664d3738edc4568e4b6398
wisuwat <wisuwatpond01@gmail.com>
2025-01-19 19:16:38 +0700

    Update Unheld Dice When Roll

--- src/App.jsx
+++ src/App.jsx
@@ -14,7 +14,11 @@ function App() {
 
     const dice = [];
     for(let i = 0; i < 10; i++) {
-      const random = {value: Math.floor(Math.random() * 6) + 1, isHeld: false, id: nanoid()}
+      const random = {
+        value: Math.floor(Math.random() * 6) + 1, 
+        isHeld: false, 
+        id: nanoid()
+      }
       dice.push(random)
     }
     return dice
@@ -31,14 +35,28 @@ function App() {
   }
 
   function reRoll () {
-    setNumber(generateAllNewDice())
+    setNumber((prev) => (
+      prev.map((d) => (
+        d.isHeld
+        ? d
+        : {...d, value: Math.floor(Math.random() * 6) + 1}
+      ))
+    ))
   }
 
   return (
     <main>
+      <h1 className='title'>Tenzies</h1>
+      <p className='instructions'>Roll until all dice are the same. Click each die to freeze it at its current value between rolls.</p>
       <div className='container'>
         {number.map((n) => (
-          <Die key={n.id} id={n.id} value={n.value} isHeld={n.isHeld} click={clickDice}/>
+          <Die 
+            key={n.id} 
+            id={n.id} 
+            value={n.value} 
+            isHeld={n.isHeld} 
+            click={clickDice}
+          />
         ))}
       </div>
       <button className='roll-dice' onClick={reRoll}>Roll</button>
