f923fa4a7357e5f458e3834223863368450a992d
wisuwat <wisuwatpond01@gmail.com>
2025-01-19 20:15:14 +0700

    Add Improvements

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useState, useRef, useEffect } from 'react';
 import './App.css'
 import Die from './components/Die'
 import { nanoid } from 'nanoid'
@@ -6,7 +6,7 @@ import Confetti from 'react-confetti'
 
 function App() {
   const [number, setNumber] = useState(() => generateAllNewDice())
-
+  const buttonRef = useRef(null)
 
   function generateAllNewDice() {
     // return new Array(10)
@@ -53,12 +53,22 @@ function App() {
   const gameWon = number.every(n => n.isHeld)
     && number.every(n => n.value === number[0].value)
 
+
+  useEffect(() => {
+    if (gameWon) {
+      buttonRef.current.focus()
+    }
+  }, [gameWon])
+  
   return (
     <main>
       {gameWon 
         ? <Confetti />
         : null
       }
+      <div aria-live='polite' className='sr-only'>
+        {gameWon && <p>Congratulations! You won! press "New Game" to start again.</p>}
+      </div>
       <h1 className='title'>Tenzies</h1>
       <p className='instructions'>Roll until all dice are the same. Click each die to freeze it at its current value between rolls.</p>
       <div className='container'>
@@ -72,7 +82,7 @@ function App() {
           />
         ))}
       </div>
-      <button className='roll-dice' onClick={reRoll}>{gameWon ? "New Game" : "Roll"}</button>
+      <button ref={buttonRef} className='roll-dice' onClick={reRoll}>{gameWon ? "New Game" : "Roll"}</button>
     </main>
   )
 }
--- src/components/Die.jsx
+++ src/components/Die.jsx
@@ -1,6 +1,11 @@
 function Die ({ value, isHeld, click, id }) {
     return (
-        <button onClick={() => click(id)} style={{ backgroundColor: isHeld && "#59E391" }}>
+        <button 
+            onClick={() => click(id)} 
+            style={{ backgroundColor: isHeld && "#59E391" }}
+            aria-pressed={isHeld}
+            aria-label={`Die with value ${value}, ${isHeld ? "held" : "not held"}`}
+        >
             {value}
         </button>
     )
